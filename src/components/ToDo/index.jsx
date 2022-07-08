e9a04aa0f4c3443cc857bb5c79e045138112f5d6
Alexandr Saleyev <saleyevav@yandex.ru>
2022-07-08 12:00:07 +0300

    change ToDo styles

--- src/components/ToDo/index.jsx
+++ src/components/ToDo/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import classes from './styles.module.css';
 
-const ToDo = ({ name, id }) => {
+const ToDo = ({ name, id, style }) => {
   return (
-    <div className={classes.wrapper} >
+    <div className={classes.wrapper} style={style}>
       <div className={classes.button} ></div>
       <span>{id}. {name}</span>
     </div>
--- src/components/ToDoList/index.jsx
+++ src/components/ToDoList/index.jsx
@@ -5,7 +5,7 @@ const ToDoList = ({ list }) => {
   return (
     <div className={classes.wrapper}>
       {list.map((toDo, index) => (
-        <ToDo key={index} id={index + 1} name={toDo.name} className={classes.item} />
+        <ToDo key={index} id={index + 1} name={toDo.name} style={{ marginBottom: 10 }} />
       ))}
     </div>
   );
