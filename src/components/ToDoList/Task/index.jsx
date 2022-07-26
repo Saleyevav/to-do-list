281fadc9c2b8cfbb34c30c15fc8bb8cf4bd852fd
Alexandr Saleyev <saleyevav@yandex.ru>
2022-07-26 15:42:35 +0300

    Rename ToDo to Task

---
+++ src/components/ToDoList/Task/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from "./styles.module.css";
+
+const Task = ({ title, id, style, completed, completeFunction }) => {
+  let textStyle = "";
+  completed ? (textStyle = "completed") : (textStyle = "notCompleted");
+  return (
+    <div className={classes.wrapper} style={style}>
+      <div
+        className={classes.button}
+        onClick={() => completeFunction(id)}
+      ></div>
+      <span className={classes[textStyle]}>
+        {id}. {title}
+      </span>
+    </div>
+  );
+};
+
+export default Task;
