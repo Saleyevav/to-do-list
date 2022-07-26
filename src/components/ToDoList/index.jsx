d8d0590574f15802ece1469c6f8b49cd11d27a8e
Alexandr Saleyev <saleyevav@yandex.ru>
2022-07-26 08:48:49 +0300

    move todo directory in todolist

--- src/components/ToDoList/index.jsx
+++ src/components/ToDoList/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import ToDo from "../ToDo";
-import classes from './styles.module.css';
+import ToDo from "./ToDo";
+import classes from "./styles.module.css";
 const ToDoList = ({ list, completeFunction }) => {
   return (
     <div className={classes.wrapper}>
       {list.map((toDo, index) => (
-        <ToDo key={index} id={index + 1} title={toDo.title} style={{ marginBottom: 10 }} completed={toDo.completed} completeFunction={completeFunction} />
+        <ToDo
+          key={index}
+          id={index + 1}
+          title={toDo.title}
+          style={{ marginBottom: 10 }}
+          completed={toDo.completed}
+          completeFunction={completeFunction}
+        />
       ))}
     </div>
   );
