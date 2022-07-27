ece04767a407ab13025c1d88fa221d3162840bf8
Alexandr Saleyev <saleyevav@yandex.ru>
2022-07-27 13:12:45 +0300

    add eventlisener for input

--- src/components/InputWithButton/index.jsx
+++ src/components/InputWithButton/index.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import classes from './styles.module.css';
+import classes from "./styles.module.css";
 const InputWithButton = React.forwardRef((props, ref) => {
-    return (
-        <div className={classes.wrapper}>
-            <div className={classes.border}>
-                <input ref={ref} type="text" className={classes.input} />
-                <div className={classes.button} onClick={props.onBtnClickFunction}>+</div>
-            </div>
+  const keydown = (e) => {
+    if (e.keyCode === 13) {
+      props.onBtnClickFunction();
+    }
+  };
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.border}>
+        <input
+          ref={ref}
+          type="text"
+          className={classes.input}
+          onKeyDown={keydown}
+        />
+        <div className={classes.button} onClick={props.onBtnClickFunction}>
+          +
         </div>
-    );
+      </div>
+    </div>
+  );
 });
 
 export default InputWithButton;
